cb1a015284b11458c6547f05589907cedd8cc697
athira <athiraomanakuttan20@gmail.com>
2025-03-18 10:11:20 +0530

    fix(token) : token time changed

--- src/utils/jwtUtility.ts
+++ src/utils/jwtUtility.ts
@@ -6,10 +6,10 @@ const access_secret = process.env.JWT_ACCESS_TOKEN_SECRET_KEY as string
 const refresh_secret = process.env.JWT_REFRESH_TOKEN_SECRET_KEY as string
 class JwtUtility{
     static generateAccessToken(payload:object):string{
-        return jwt.sign(payload,access_secret,{expiresIn:'1m'})
+        return jwt.sign(payload,access_secret,{expiresIn:'1h'})
     }
     static generateRefreshToken(payload:object):string{
-        return jwt.sign(payload, refresh_secret,{expiresIn:'1d'})
+        return jwt.sign(payload, refresh_secret,{expiresIn:'2d'})
     }
     static verifyToken(token: string, isRefreshToken = false): string | JwtPayload {
         const secret = isRefreshToken ? refresh_secret : access_secret;
